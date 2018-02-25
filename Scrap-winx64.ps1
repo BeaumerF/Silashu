5be5096602e723c5405f976bd410415eb843feb6
BeaumerF <tit_shadow_shao@hotmail.fr>
2018-02-25 18:34:09 +0100

    add powershell script

---
+++ Scrap-winx64.ps1
@@ -0,0 +1,10 @@
+﻿$array = new-object System.Collections.ArrayList
+
+DO
+{
+write-host "Do you want to add Google Search Link ?"
+$link =  read-host
+if ($link) { $array.Add($link) }
+} While ($link)
+
+dotnet.exe .\Silashu\bin\Release\PublishOutput\EmailScraper.dll $array
